f24c6e5e4199f265ae9b2c469977485adf882f02
ikonon <315793076@qq.com>
2022-12-24 17:46:15 +0800

    cli: lint

---
+++ scripts/read-hook.mjs
@@ -0,0 +1,7 @@
+import fs from 'fs';
+import path from 'path';
+
+const res = fs.readFileSync(path.resolve('../', '.git/hooks/pre-commit'));
+
+const b= res.toString();
+console.log('b', b);
--- scripts/watch-main.mjs
+++ scripts/watch-main.mjs
@@ -1,9 +1,9 @@
-import {build, createServer} from 'vite';
+import {build} from 'vite';
 import path from 'path';
 import {spawn} from 'child_process';
-import electronPath from 'electron';
+// import electronPath from 'electron';
 
-const mode = process.env.MODE = process.env.MODE || 'development'
+const mode = (process.env.MODE = process.env.MODE || 'development');
 // const server = await createServer({
 //   mode,
 //   configFile: path.resolve('../packages/renderer/vite.config.js'),
@@ -11,30 +11,26 @@ const mode = process.env.MODE = process.env.MODE || 'development'
 //     port: 8888
 //   }
 // }).then(s => s.listen())
-console.log('开始', 'spawn')
-const sv = spawn(/^win/.test(process.platform)?'npm.cmd' : 'npm', ['-v'], {
-  stdio: 'inherit'
-})
+console.log('开始', 'spawn');
+const sv = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['-v'], {
+  stdio: 'inherit',
+});
 
 // sv.addListener('data', d => console.log(d.toString()));
 
-sv.addListener('error', (err) => {
-  console.error(err)
-})
-console.log('hhh', 'xxx')
+sv.addListener('error', err => {
+  console.error(err);
+});
+console.log('hhh', 'xxx');
 // setPreload()
 /**
  * 建立文件观察器
  * @param {import('vite').ViteDevServer} watchServer
  */
 function setPreload() {
-  const bu = build({
+  return  build({
     mode,
     configFile: path.resolve('../packages/renderer/vite.config.js'),
-  })
-  bu.then(res=> {
-    res
-  }).catch(err => {
-    console.error(err)
-  })
+  });
+
 }
